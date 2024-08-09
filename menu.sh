3d7145484f6bae3ade02e5b8a96abd01ffee81e6
Paulo <127901575+UlekBR@users.noreply.github.com>
2024-08-09 19:26:09 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -39,7 +39,7 @@ nome_do_script="sshmanagerapi"
 # Loop principal
 while true; do
     clear
-    echo -e "S-S-H--M-A-N-A-G-E-R  Versão: 0.0.3 - BETA"
+    echo -e "S-S-H--M-A-N-A-G-E-R  Versão: 0.0.4 - BETA"
     echo -e "By @UlekBR"
 
     if verificar_processo "$nome_do_script"; then
